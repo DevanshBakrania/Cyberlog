d203d5971564adf34e1d5298186661ad343b476b
DevanshBakrania <devanshbakrania1177@gmail.com>
2025-12-05 16:32:05 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,73 @@
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(const MyFirstApp());
+  runApp(const MyApp());
 }
-class MyFirstApp extends StatelessWidget {
-  const MyFirstApp({super.key});
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("If Lost, Contact"),
-        ),
-        body: const Center(
-          child: Text(
-            "Name : Devansh \n Phone : +91 99678 92886",
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 22),
-          ),
+      debugShowCheckedModeBanner: false,
+      home: const EvenOddChecker(),
+    );
+  }
+}
+
+class EvenOddChecker extends StatefulWidget {
+  const EvenOddChecker({super.key});
+
+  @override
+  State<EvenOddChecker> createState() => _EvenOddCheckerState();
+}
+
+class _EvenOddCheckerState extends State<EvenOddChecker> {
+  final TextEditingController numberController = TextEditingController();
+  String resultMessage = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Even / Odd Checker"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              controller: numberController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: "Enter a number",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                String input = numberController.text;
+                int number = int.parse(input);
+                if (number % 2 == 0) {
+                  resultMessage = "The number $number is Even.";
+                } else {
+                  resultMessage = "The number $number is Odd.";
+                }
+                setState(() {});
+              },
+              child: const Text("Check"),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              resultMessage,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
