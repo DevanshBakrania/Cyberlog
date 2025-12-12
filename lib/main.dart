a55ae0a4ee5c36f03f042541daa74e12443f2f0b
DevanshBakrania <devanshbakrania1177@gmail.com>
2025-12-12 17:22:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,87 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CyberLogApp());
+  runApp(const MyApp());
 }
 
-class CyberLogApp extends StatelessWidget {
-  const CyberLogApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "CyberLog",
-      home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
+      home: const CyberLogDashboard(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class CyberLogDashboard extends StatelessWidget {
+  const CyberLogDashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("CyberLog"),
+        backgroundColor: Colors.blueGrey.shade900,
+        title: const Text(
+          "CyberLog Dashboard !!",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey,
+          ),
+        ),
+        centerTitle: true,
       ),
+
       body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        padding: const EdgeInsets.all(16),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 15,
+          mainAxisSpacing: 15,
+
           children: [
-            const Text(
-              "Home Dashboard !!",
-              style: TextStyle(fontSize: 25),
-            ),
-
-            const SizedBox(height: 10),
-
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.blue.shade100,
-              ),
-              child: const Text(
-                "View Logs",
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-
-            const SizedBox(height: 20),
-
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: Colors.green.shade100,
-              ),
-              child: const Text(
-                "System Status",
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
+            dashCard(Icons.book, "Daily Log"),
+            dashCard(Icons.lightbulb, "Cyber Tips"),
+            dashCard(Icons.security, "Device Security"),
+            dashCard(Icons.note, "Notes"),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget dashCard(IconData icon, String title) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blueGrey.shade100,
+        borderRadius: BorderRadius.circular(30),
+
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade400,
+            blurRadius: 6,
+            spreadRadius: 1,
+            offset: const Offset(2, 6),
+          ),
+        ],
+      ),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 50, color: Colors.blueGrey.shade900),
+          const SizedBox(height: 12),
+          Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
       ),
     );
   }
