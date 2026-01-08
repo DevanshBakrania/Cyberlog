a16940e21ff28661e0bc5c7e5f698b8b7655c84e
DevanshBakrania <devanshbakrania1177@gmail.com>
2026-01-08 13:11:20 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/cyberlog/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/cyberlog/MainActivity.kt
@@ -1,5 +1,32 @@
 package com.example.cyberlog
 
+import android.os.Build
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private val channel = "cyberlog/device"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            channel
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getDeviceModel" -> {
+                    result.success(Build.MODEL)
+                }
+                "getAndroidVersion" -> {
+                    result.success(Build.VERSION.RELEASE)
+                }
+                else -> {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+}
